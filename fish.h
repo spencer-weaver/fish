56c2459b1100baaca2c3379abda8f9076c79449a
Spencer Weaver <sweavertheslayr@outlook.com>
2024-06-03 21:43:45 -0400

    started file layout

---
+++ fish.h
@@ -0,0 +1,33 @@
+#ifndef FISH
+#define FISH
+
+class Fish {
+
+
+	// agnatha
+	// lack jaws and paired fins
+
+	// chondrichthyes
+	// a cartilaginous skeleton, lack of a swim bladder, lipid storage restricted to the liver, unique hematopoietic organs, and bacteremia in clinically normal individuals
+
+	// osteichthyes
+	// a skeleton of bone, scales, paired fins, one pair of gill openings, jaws, and paired nostrils
+
+};
+
+class Osteichthyes {
+
+	// 9mm-11m
+	//
+	//			-----------------
+	//        /					  \
+	//        \					  /
+	//          -----------------
+	//
+	//
+
+
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
-#include "Game.h"
 
 int main(int argc, char** argv) {
-    Uint32 frameStart;
-    int frameTime;
 
-    Game* game = new Game();
-    if (game->init() == false) return 1;
+    while (true) {
 
-    while (game->running()) {
-        frameStart = SDL_GetTicks();
-        
-        game->handleEvents();
-        game->update();
-        game->render();
+        // handle input
 
-        frameTime = SDL_GetTicks() - frameStart;
+        // update game objects
 
-        if (FRAME_DELAY > frameTime) {
-            SDL_Delay(FRAME_DELAY - frameTime);
-        }
+        // render updates
+
     }
 
-    game->quit();
 
     return 0;
 }
